40091f523c2e903aec43f3a1ec84c70b7bc14cf2
Muhammad Hafizoddin Bin Roslan <>
2023-09-25 19:54:53 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
-import { FooterComponents } from './Common';
+import { FooterComponents,NavbarComponents } from './Common';
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
+    <div>
+        <NavbarComponents/>
+          <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      
+          <p className="text-3xl text-gray-700 font-bold mb-5">
+            Welcome!
+          </p>
+          <p className="text-gray-500 text-lg">
+            React and Tailwind CSS in action
+          </p>
 
-      <FooterComponents/>
+          
+        </div>
+        <FooterComponents/>
     </div>
+
 
   );
 }
--- src/Common/index.js
+++ src/Common/index.js
@@ -1,1 +1,2 @@
 export { default as FooterComponents } from './components/FooterComponents';
+export { default as NavbarComponents } from './components/NavbarComponents';
